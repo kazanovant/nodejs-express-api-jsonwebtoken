c9148dc812d5080d879710dfddbacb7b39340d23
KOMPTUK-24 <camzgenius@gmail.com>
2022-06-11 10:52:51 +0700

    second commit

--- src/configs/database.js
+++ src/configs/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 module.exports = {
     multipleStatements  : true,
     host                : "sql6.freemysqlhosting.net",
-    user                : "sql6498920",
-    password            : "atwEmgTqwj",
-    database            : "sql6498920"
+    user                : "sql6499024",
+    password            : "ztDgGx5R6D",
+    database            : "sql6499024"
 };
